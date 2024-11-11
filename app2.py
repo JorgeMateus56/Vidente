c0d05dff5d1cc38202a4c39795f49b99a9f62ba4
JorgeMateus56 <jorge.mateus@gmail.com>
2024-11-11 17:50:49 -0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,10 +4,10 @@ import gdown
 @st.cache_resource
 def carrega_modelo():
   # https://drive.google.com/file/d/1vCvzakWS3EqmPJTRmEHxsyryvnWFRVs9/view?usp=sharing
-  url = 'https://drive.google.com/file/d/1vCvzakWS3EqmPJTRmEHxsyryvnWFRVs9/view?usp=sharing'
+  url = 'https://drive.google.com/uc?id=1vCvzakWS3EqmPJTRmEHxsyryvnWFRVs9'
   gdown.download(url,'modelo_vidente.keras')
   # https://drive.google.com/file/d/1HZ5e1X4whC8PIcPMOcJX43x55_Li03kI/view?usp=sharing
-  url = 'https://drive.google.com/file/d/1HZ5e1X4whC8PIcPMOcJX43x55_Li03kI/view?usp=sharing'
+  url = 'https://drive.google.com/uc?id=1HZ5e1X4whC8PIcPMOcJX43x55_Li03kI'
   gdown.download(url,'vectorizer.pkl')
   loaded_model = tf.keras.models.load_model('modelo_vidente.keras')
   with open('vectorizer.pkl','rb') as file:
