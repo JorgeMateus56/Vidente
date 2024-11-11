38545b45d46976307e9aed4c65e28ad8be5a5928
JorgeMateus56 <jorge.mateus@gmail.com>
2024-11-11 17:15:32 -0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,1 +1,12 @@
-# aplicacao streamlit
+import streamlit as st
+def main():
+  max_sequence_len=50
+
+  # Carregar Modelo
+
+  st.title('Previsão de próximas palavras')
+  input_text = st.text_input('Digite uma sequencia de texto:')
+
+if __name__=='__main__':
+  main()
+    
