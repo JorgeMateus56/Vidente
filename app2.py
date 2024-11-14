37ba1f94e7902cdda5d593fe2318ece8c74714fb
JorgeMateus56 <jorge.mateus@gmail.com>
2024-11-14 16:03:41 -0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -11,8 +11,8 @@ def carrega_modelo():
   url = 'https://drive.google.com/uc?id=1vCvzakWS3EqmPJTRmEHxsyryvnWFRVs9'
   gdown.download(url,'modelo_vidente.keras')
   # https://drive.google.com/file/d/1HZ5e1X4whC8PIcPMOcJX43x55_Li03kI/view?usp=sharing
-  url = 'https://drive.google.com/uc?id=1HZ5e1X4whC8PIcPMOcJX43x55_Li03kI'
-  gdown.download(url,'vectorizer.pkl')
+  #url = 'https://drive.google.com/uc?id=1HZ5e1X4whC8PIcPMOcJX43x55_Li03kI'
+  #gdown.download(url,'vectorizer.pkl')
   loaded_model = tf.keras.models.load_model('modelo_vidente.keras')
   with open('vectorizer.pkl','rb') as file:
     vectorizer = pickle.load(file)
